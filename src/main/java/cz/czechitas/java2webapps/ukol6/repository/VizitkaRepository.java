361aad99750786ed8745036dce6d3b61c568ce64
Dasa <dasa.podkulova@gmail.com>
2024-06-18 12:56:54 +0200

    Dodělaný úkol

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/repository/VizitkaRepository.java
@@ -0,0 +1,12 @@
+package cz.czechitas.java2webapps.ukol6.repository;
+
+import cz.czechitas.java2webapps.ukol6.entity.Vizitka;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VizitkaRepository extends CrudRepository<Vizitka, Integer> {
+
+}
